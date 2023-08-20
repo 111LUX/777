62227de6aad0382404b3e145bbf9e01dd30ebfef
111LUX <135356307+111LUX@users.noreply.github.com>
2023-08-20 19:00:54 +0300

    Update vol.sh

--- vol.sh
+++ vol.sh
@@ -5,4 +5,4 @@ case $@ in
 	down) VOL="$(amixer sset Master 7%- unmute | grep -Po "[0-9]+(?=%)" | head -1)" ;;
 esac
 
-echo "Vol $VOL%" | dzen2 -p 1 -bg gray4 -fg white -fn -13:Bold -h 27
+echo "Vol $VOL%" | dzen2 -p 1 -bg gray4 -fg white -fn -14:Bold -h 30
